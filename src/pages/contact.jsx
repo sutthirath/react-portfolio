5e2f440c7f32167376f298db32491e6392d44941
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-04-07 13:32:45 -0700

    test2 for redirect

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,104 +2,82 @@ import React from "react";
 import "./contact.css";
 import Success from "../pages/success";
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      confirm: false
-    };
-  }
-
-  componentWillUnmount() {
-    this.setState({
-      confirm: true
-    });
-  }
-
-  render() {
-    if (this.state.confirm) return <Success />;
-    return (
-      <div>
-        <div className="contact-links">
-          <a
-            href="https://github.com/sutthirath"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="contact-btn">
-              <i className="fab fa-github" />
-            </button>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/seanvilaysane/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="contact-btn">
-              <i className="fab fa-linkedin-in" />
-            </button>
-          </a>
-          <a href="mailto:seanvilaysane@gmail.com" target="_top">
-            <button className="contact-btn">
-              <i className="far fa-envelope" />
-            </button>
-          </a>
-        </div>
-        <section>
-          <div className="contact-form">
-            <form
-              method="post"
-              name="portfolio-msg"
-              data-netlify="true"
-              netlify-honeypot="bot-field"
-            >
-              <input type="hidden" name="form-name" value="portfolio-msg" />
-              <label className="hidden">
-                Don’t fill this out if you're human:
-                <input className="contact-input" type="text" name="bot-field" />
-              </label>
-              <label>
-                Name:
-                <input
-                  className="contact-input"
-                  type="text"
-                  name="name"
-                  required
-                />
-              </label>
-              <label>
-                Company:
-                <input className="contact-input" type="text" name="company" />
-              </label>
-              <label>
-                Email:
-                <input
-                  className="contact-input"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </label>
-              <label>
-                Phone:
-                <input className="contact-input" type="tel" name="phone" />
-              </label>
-              <textarea
-                className="contact-textarea"
-                placeholder="send me a message"
-                name="message"
-                required
-              />
-              <div data-netlify-recaptcha="true" />
-              <button className="send-btn" type="submit">
-                Send
-              </button>
-            </form>
-          </div>
-        </section>
+const Contact = () => (
+  <div>
+    <div className="contact-links">
+      <a
+        href="https://github.com/sutthirath"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="contact-btn">
+          <i className="fab fa-github" />
+        </button>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/seanvilaysane/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="contact-btn">
+          <i className="fab fa-linkedin-in" />
+        </button>
+      </a>
+      <a href="mailto:seanvilaysane@gmail.com" target="_top">
+        <button className="contact-btn">
+          <i className="far fa-envelope" />
+        </button>
+      </a>
+    </div>
+    <section>
+      <div className="contact-form">
+        <form
+          method="post"
+          name="portfolio-msg"
+          action={<Success />}
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="portfolio-msg" />
+          <label className="hidden">
+            Don’t fill this out if you're human:
+            <input className="contact-input" type="text" name="bot-field" />
+          </label>
+          <label>
+            Name:
+            <input className="contact-input" type="text" name="name" required />
+          </label>
+          <label>
+            Company:
+            <input className="contact-input" type="text" name="company" />
+          </label>
+          <label>
+            Email:
+            <input
+              className="contact-input"
+              type="email"
+              name="email"
+              required
+            />
+          </label>
+          <label>
+            Phone:
+            <input className="contact-input" type="tel" name="phone" />
+          </label>
+          <textarea
+            className="contact-textarea"
+            placeholder="send me a message"
+            name="message"
+            required
+          />
+          <div data-netlify-recaptcha="true" />
+          <button className="send-btn" type="submit">
+            Send
+          </button>
+        </form>
       </div>
-    );
-  }
-}
+    </section>
+  </div>
+);
 
 export default Contact;
